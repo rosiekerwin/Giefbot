51d45f7a5a5b37d41e23c7f112132e60b454cf19
Zarkdion <hodenazarkion@gmail.com>
2017-12-07 16:42:12 -0500

    First Commit of new branch
    Attempted to re-integrate the work done by initialize() back into
    update_q() and magic()

--- giefbot.py
+++ giefbot.py
@@ -37,7 +37,9 @@ def main():
     D = []
     step = 0
     rate = 1
-    sess, output_net, x, cost, trainer, mask, reward, nextQ = initialize()
+    isFirst = True
+    #sess, output_net, x, cost, trainer, mask, reward, nextQ = initialize()
+    sess = tf.Session()
     for i in range(4):
         observation, rw, done, info = env.step(action)  # pass in 0 for action
         observation = convert_to_small_and_grayscale(observation)
@@ -55,16 +57,17 @@ def main():
             D.pop()
         if step % 100 == 0:
             rate = rate / 2
-        action = magic(curr_obs, sess, output_net, x,step,rate) #change this to just take in curr_obs, sess, and False
+        action = magic(curr_obs, sess, step,rate, isFirst) #change this to just take in curr_obs, sess, and False
         env.render()
         observation, rw, done, info = env.step(action) # take a random action
-        print(action, rw)
+        #print(action, rw)
         observation = convert_to_small_and_grayscale(observation)
         e = [rw, action, prev_obs, curr_obs]
         D.append(e)
         prev_obs = curr_obs
         curr_obs = obsUpdate(curr_obs,observation)
-        update_q_function(D, sess, output_net, x, cost, trainer, mask, reward, nextQ)
+        update_q_function(D, sess, isFirst)
+        isFirst = False
 
 def save(sess):
     saver = tf.train.Saver()
@@ -76,10 +79,48 @@ def load(sess, save_path):
     saver.restore(sess, save_path)
     print("Model restored.")
 
-def update_q_function(D, sess, output_net, x, cost, trainer, mask, reward, nextQ):
+def update_q_function(D, sess, isFirst):
     gamma = 0.99
     T = 4
     indexes = []
+    
+    x = tf.placeholder(tf.float32, shape = [4,84,84])
+    #x = tf.placeholder(tf.float32, shape = [84,84,4])
+
+    W_conv1 = weight_variable([8,8,4,32])
+    b_conv1 = bias_variable([32])
+    x_image = tf.reshape(x,[-1,84,84,4])
+    h_conv1 = tf.nn.relu(conv2d(x_image,W_conv1, 4)+b_conv1)
+
+    W_conv2 = weight_variable([4, 4, 32, 64])
+    b_conv2 = bias_variable([64])
+    h_conv2 = tf.nn.relu(conv2d(h_conv1, W_conv2, 2) + b_conv2)
+
+    W_conv3 = weight_variable([3, 3, 64, 64])
+    b_conv3 = bias_variable([64])
+    h_conv3 = tf.nn.relu(conv2d(h_conv2, W_conv3, 1) + b_conv3)
+    conv_out = tf.reshape(h_conv3, [-1, 11*11*64]) # this is the line that was giving us issues.
+
+    w_hidden = weight_variable([7744,512])
+    b_hidden = bias_variable([512])
+    hidden_net = tf.matmul(conv_out,w_hidden)+b_hidden
+    hidden_out = tf.nn.relu(hidden_net)
+
+    w_output = weight_variable([512,num_actions])
+    b_output = bias_variable([num_actions])
+    output_net = tf.matmul(hidden_out,w_output)+b_output
+    
+    reward = tf.placeholder(tf.float32, shape = [1])
+    nextQ = tf.placeholder(tf.float32, shape = [1])
+    mask = tf.placeholder(tf.float32, shape = [num_actions])
+    cost = (((mask*reward)+(mask*nextQ))-mask*output_net)
+    #print(type(cost))
+    #print(tf.trainable_variables())
+    trainer = tf.train.AdamOptimizer(learning_rate=0.0001).minimize(cost)
+    
+    if isFirst:
+        sess.run(tf.global_variables_initializer())
+    
     for _ in range(T):
         indexes.append(D[rand.randrange(len(D))])
     
@@ -113,8 +154,47 @@ def bias_variable(rshape):
 def conv2d(x, W, st):
   return tf.nn.conv2d(x, W, strides=[1, st, st, 1], padding='SAME')
 
-def initialize():
-    sess = tf.Session()
+# def initialize():
+#     sess = tf.Session()
+#     x = tf.placeholder(tf.float32, shape = [4,84,84])
+#     #x = tf.placeholder(tf.float32, shape = [84,84,4])
+# 
+#     W_conv1 = weight_variable([8,8,4,32])
+#     b_conv1 = bias_variable([32])
+#     x_image = tf.reshape(x,[-1,84,84,4])
+#     h_conv1 = tf.nn.relu(conv2d(x_image,W_conv1, 4)+b_conv1)
+# 
+#     W_conv2 = weight_variable([4, 4, 32, 64])
+#     b_conv2 = bias_variable([64])
+#     h_conv2 = tf.nn.relu(conv2d(h_conv1, W_conv2, 2) + b_conv2)
+# 
+#     W_conv3 = weight_variable([3, 3, 64, 64])
+#     b_conv3 = bias_variable([64])
+#     h_conv3 = tf.nn.relu(conv2d(h_conv2, W_conv3, 1) + b_conv3)
+#     conv_out = tf.reshape(h_conv3, [-1, 11*11*64]) # this is the line that was giving us issues.
+# 
+#     w_hidden = weight_variable([7744,512])
+#     b_hidden = bias_variable([512])
+#     hidden_net = tf.matmul(conv_out,w_hidden)+b_hidden
+#     hidden_out = tf.nn.relu(hidden_net)
+# 
+#     w_output = weight_variable([512,num_actions])
+#     b_output = bias_variable([num_actions])
+#     output_net = tf.matmul(hidden_out,w_output)+b_output
+#     
+#     #currentQ = tf.Variable(tf.constant(0.1, shape=[6]))
+#     reward = tf.placeholder(tf.float32, shape = [1])
+#     nextQ = tf.placeholder(tf.float32, shape = [1])
+#     mask = tf.placeholder(tf.float32, shape = [num_actions])
+#     cost = (((mask*reward)+(mask*nextQ))-mask*output_net)
+#     #print(type(cost))
+#     #print(tf.trainable_variables())
+#     trainer = tf.train.AdamOptimizer(learning_rate=0.0001).minimize(cost)
+#     sess.run(tf.global_variables_initializer())
+#     return sess, output_net, x, cost, trainer, mask, reward, nextQ
+
+def magic(curr_obs, sess, step, rate, isFirst):
+    
     x = tf.placeholder(tf.float32, shape = [4,84,84])
     #x = tf.placeholder(tf.float32, shape = [84,84,4])
 
@@ -141,19 +221,10 @@ def initialize():
     b_output = bias_variable([num_actions])
     output_net = tf.matmul(hidden_out,w_output)+b_output
     
-    #currentQ = tf.Variable(tf.constant(0.1, shape=[6]))
-    reward = tf.placeholder(tf.float32, shape = [1])
-    nextQ = tf.placeholder(tf.float32, shape = [1])
-    mask = tf.placeholder(tf.float32, shape = [num_actions])
-    cost = (((mask*reward)+(mask*nextQ))-mask*output_net)
-    #print(type(cost))
-    #print(tf.trainable_variables())
-    trainer = tf.train.AdamOptimizer(learning_rate=0.0001).minimize(cost)
-    sess.run(tf.global_variables_initializer())
-    return sess, output_net, x, cost, trainer, mask, reward, nextQ
-
-def magic(curr_obs, sess, output_net, x,step,rate):
-    var = sess.run(output_net, feed_dict={x: curr_obs})
+    if isFirst:
+        sess.run(tf.global_variables_initializer())
+    
+    var = sess.run([output_net], feed_dict={x: curr_obs})
     prediction_index, predicted_value = max(enumerate(var), key=operator.itemgetter(1))
     #print(var)
     
